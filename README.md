cf9926816b0b889cf94dc4862f1ac293846a8465
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-10 21:13:51 +0300

    The script displays information about the availability of the site. Displays

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+The script displays information about the availability of the site.
+Displays the site online or not, and the domain payment period has expired or not
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```#!bash
+
+pip install -r requirements.txt # alternatively try pip3
+
+python check_sites_health.py
+
+Укажите путь к файлу: site.txt
+Сервер:http://ya.ru -  доступен(True) и домен оплачен(True)
+Сервер:http://mail.ru -  доступен(True) и домен оплачен(True)
+
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,46 @@
+from whois import whois
+import requests
+import datetime
+import sys
+import os
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as url_server_file:
+        url_server_list = url_server_file.read().split()
+    return url_server_list
+
 
 def is_server_respond_with_200(url):
-    pass
+    return requests.get(url).ok
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    return whois(domain_name)['expiration_date']
+
+
+def is_domain_paid(domain_name, period_of_time):
+    days_left = get_domain_expiration_date(domain_name) - datetime.datetime.today()
+    if days_left > datetime.timedelta(days=period_of_time):
+        return True
+    return days_left
+
+
+def main():
+    url_server_list = load_urls4check(file_path)
+    days_left = 30
+    for url in url_server_list:
+        site_status = is_server_respond_with_200(url)
+        domain_paid = is_domain_paid(url, days_left)
+        print('Сервер:{} -  доступен({}) и домен оплачен({})'.format(url, site_status, domain_paid))
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        file_path = input('Укажите путь к файлу: ')
+    if os.path.exists(file_path):
+        main()
+    else:
+        print('Неверный путь к файлу')
